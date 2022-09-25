4e833099a140ee407ea1466a33869c4de40342db
demhack5 <114308453+demhack5@users.noreply.github.com>
2022-09-25 17:25:18 +0300

    class implement

--- users_data.py
+++ users_data.py
@@ -2,102 +2,109 @@ import sqlite3 as sql
 from tasks_data import *
 
 N_STAGES = 3
-DATA_BASE_NAME = 'users_data'
-DATABASE_PATH  = '../'
 
-def init_table():
-    conn = sql.connect('%s/%s.db' % (DATABASE_PATH, DATA_BASE_NAME))
+class users_data(object):
 
-    cur = conn.cursor()
-    table_init_str = """CREATE TABLE IF NOT EXISTS %s(
-            user_id INT PRIMARY KEY,
-            cur_stage     INT,""" % (DATA_BASE_NAME)
+    def __init__(self, db_path):
 
-    for n_stage in range(N_STAGES - 1):
-        table_init_str += "stage%d INT, " % (n_stage)
+        self.path  = db_path + '/users_data.db'
+        self.tasks = tasks_data(db_path)
 
-    table_init_str += "stage%d INT" % (N_STAGES - 1)
-    table_init_str += ");"
+        onlyfiles = [f for f in listdir(db_path) if isfile(join(db_path, f))]
     
-    cur.execute(table_init_str)
-    conn.commit()
+        if not 'tasks' in onlyfiles:
+            conn = sql.connect(self.path)
 
-    return
+            cur = conn.cursor()
+            table_init_str = """CREATE TABLE IF NOT EXISTS users_data(
+                    user_id INT PRIMARY KEY,
+                    cur_stage     INT,"""
 
-def get_cur_stage(user_id):
+            for n_stage in range(N_STAGES - 1):
+                table_init_str += "stage%d INT, " % (n_stage)
 
-    conn = sql.connect('%s/%s.db' % (DATABASE_PATH, DATA_BASE_NAME))
-    cur  = conn.cursor()
+            table_init_str += "stage%d INT" % (N_STAGES - 1)
+            table_init_str += ");"
+            
+            cur.execute(table_init_str)
+            conn.commit()
 
-    cur_stage = cur.execute("SELECT cur_stage FROM %s WHERE user_id = %d" % (DATA_BASE_NAME, user_id)).fetchall()
+    def get_cur_stage(self, user_id):
 
-    conn.commit()
-    
-    return cur_stage[0][0]
+        conn = sql.connect(self.path)
+        cur  = conn.cursor()
 
-def get_cur_actions(user_id):
-    conn = sql.connect('%s/%s.db' % (DATABASE_PATH, DATA_BASE_NAME))
-    cur  = conn.cursor()
+        cur_stage = cur.execute("SELECT cur_stage FROM users_data WHERE user_id = %d" % (user_id)).fetchall()
 
-   # TODO: optimize
+        conn.commit()
+        
+        return cur_stage[0][0]
 
-    cur_actions = []
-    cur_stage = get_cur_stage(user_id)
+    def get_cur_actions(self, user_id):
+        conn = sql.connect(self.path)
+        cur  = conn.cursor()
 
-    for n_action in range(cur_stage + 1):
-        cur_actions.append(cur.execute("SELECT stage%d FROM %s WHERE user_id = %d" % (n_action, DATA_BASE_NAME, user_id)).fetchall()[0][0])
-    
-    conn.commit()
+    # TODO: optimize
 
-    return cur_actions
+        cur_actions = []
+        cur_stage = self.get_cur_stage(user_id)
 
-def check_user_in_database(user_id):
-    
-    conn = sql.connect('%s/%s.db' % (DATABASE_PATH, DATA_BASE_NAME))
-    cur  = conn.cursor()
+        for n_action in range(cur_stage + 1):
+            cur_actions.append(cur.execute("SELECT stage%d FROM users_data WHERE user_id = %d" % (n_action, user_id)).fetchall()[0][0])
+        
+        conn.commit()
 
-    user_data = cur.execute("SELECT * FROM %s WHERE user_id = %d" % (DATA_BASE_NAME, user_id)).fetchall()
-    conn.commit()
+        return cur_actions
 
-    if(len(user_data) > 0):
+    def check_user_in_database(self, user_id):
+        
+        conn = sql.connect(self.path)
+        cur  = conn.cursor()
+
+        user_data = cur.execute("SELECT * FROM users_data WHERE user_id = %d" % (user_id)).fetchall()
+        conn.commit()
+
+        if(len(user_data) > 0):
+            return
+        
+        cmd_str = "INSERT INTO users_data VALUES(" + str(user_id) + ", -1, "
+
+        for n_stage in range(N_STAGES - 1):
+            cmd_str += "0, "
+        cmd_str += '0)'
+
+        cur.execute(cmd_str)
+        conn.commit()
+
         return
+
+    def add_action_to_user(self, user_id, n_action, stage):
+        conn = sql.connect(self.path)
+        cur  = conn.cursor()
+
+        cur.execute("UPDATE users_data SET stage%d=%d WHERE user_id=%d" % (stage, n_action, user_id))
+        cur.execute("UPDATE users_data SET cur_stage=%d WHERE user_id=%d" % (stage, user_id))
+
+        conn.commit()
+
+        return
+
+    def get_next_action_text(self, user_id):
     
-    cmd_str = "INSERT INTO %s VALUES(" % (DATA_BASE_NAME) + str(user_id) + ", -1, "
+        cur_stage   = self.get_cur_stage(user_id)
+        cur_actions = self.get_cur_actions(user_id)
 
-    for n_stage in range(N_STAGES - 1):
-        cmd_str += "0, "
-    cmd_str += '0)'
+        for n_action in range(self.tasks.n_available_actions()):
+            if not (n_action in cur_actions):
+                self.add_action_to_user(user_id, n_action, cur_stage + 1)
+                return self.tasks.get_action_text(n_action)
+        # TODO: remove
+        return "you have done all available actions"
 
-    cur.execute(cmd_str)
-    conn.commit()
-
-    return
-
-def add_action_to_user(user_id, n_action, stage):
-    conn = sql.connect('%s/%s.db' % (DATABASE_PATH, DATA_BASE_NAME))
-    cur  = conn.cursor()
-
-    print()
-    cur.execute("UPDATE %s SET stage%d=%d WHERE user_id=%d" % (DATA_BASE_NAME, stage, n_action, user_id))
-    cur.execute("UPDATE %s SET cur_stage=%d WHERE user_id=%d" % (DATA_BASE_NAME, stage, user_id))
-
-    conn.commit()
-
-    return
-
-def get_next_action_text(user_id):
-   
-    cur_stage   = get_cur_stage(user_id)
-    cur_actions = get_cur_actions(user_id)
-
-    for n_action in range(n_available_actions()):
-        if not (n_action in cur_actions):
-            add_action_to_user(user_id, n_action, cur_stage + 1)
-            return get_action_text(n_action)
-    # TODO: remove
-    return "you have done all available actions"
-
-def validate_last_action(user_id):
-    
-    # TODO: !!!
-    return True
+    def validate_last_action(self, user_id):
+        
+        # TODO: !!!
+        return True
+        
+    def get_registration_link(self, user_id):
+        return "https:://registrate"
