f7829deefa69a2b87c86c1b733a26d6cdfcaa013
demhack5 <114308453+demhack5@users.noreply.github.com>
2022-09-25 17:25:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,8 @@ from users_data import *
 TOKEN = open('TOKEN.txt').read()
 bot = telebot.TeleBot(TOKEN)
 
-init_table()
-# id корневого квеста
-ROOT_ID = 10000
 
-ROOT_USER_ID = ...
-
-def get_registration_link(user_id):
-    return "https:://registrate"
+users = users_data('..')
 
 @bot.message_handler(commands=['start', 'begin'])
 def start_handler(msg):
@@ -26,22 +20,21 @@ def text_handler(msg):
 
     user_id = msg.from_user.id
 
-    check_user_in_database(user_id)
+    users.check_user_in_database(user_id)
     if msg.text == GET_ACTION_TEXT:
         
         msg_text = ""
 
-        if validate_last_action(user_id):
-            if get_cur_stage(user_id) == N_STAGES - 1:
-                msg_text = "Прекрасно! Теперь ты можешь зарегистироваться на наш ресурс %s" % (get_registration_link(user_id))
+        if users.validate_last_action(user_id):
+            if users.get_cur_stage(user_id) == N_STAGES - 1:
+                msg_text = "Прекрасно! Теперь ты можешь зарегистироваться на наш ресурс %s" % (users.get_registration_link(user_id))
                 bot.send_message(user_id, text = msg_text)
             else:
-                msg_text = get_next_action_text(user_id)
+                msg_text = users.get_next_action_text(user_id)
                 bot.send_message(user_id, text = msg_text, reply_markup = get_action_markup)
         else:
-            msg_text = "Валидация провалена, повторите\n\n" + get_next_action_text
+            msg_text = "Валидация провалена, повторите\n\n" + users.get_next_action_text(user_id)
             bot.send_message(user_id, text = msg_text, reply_markup = get_action_markup)
-
     return
 
 print('connecting')
