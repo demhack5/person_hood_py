c79ffc69969720ff98afb9f7509836e2172d7430
demhack5 <114308453+demhack5@users.noreply.github.com>
2022-09-25 16:47:37 +0300

    Delete scripts directory

--- scripts/start.sh
+++
@@ -1,4 +0,0 @@
-ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; cd .. ; pwd -P )" 
-export PYTHONPATH=${PYTHONPATH}:$ROOT
-$ROOT/env/bin/python3 $ROOT/demhack/bot.py
-echo "[DEBUG] Bot is offline" >> $ROOT/demhack/logs.txt
